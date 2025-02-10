c820daa7637050ec12237ca5e5d997fd82004ae2
Cwalt2 <chris.waltersts@gmail.com>
2025-02-10 07:57:40 -0600

    Deleted broken files
    files left over from broken build deleted. corrected other errors.

--- Selu383.SP25.Api/Program.cs
+++ Selu383.SP25.Api/Program.cs
@@ -14,7 +14,7 @@ namespace Selu383.SP25.Api
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<DbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 
             // Add services to the container.
 
