2977571a27fc53a4f4b461a10cc21cceb4df75b2
Cwalt2 <chris.waltersts@gmail.com>
2025-02-10 07:49:29 -0600

    Improvements to DataContext
    added things to improve sql server insertion and prepping for theaterdto implementation.

--- Selu383.SP25.Api/DataContext.cs
+++ Selu383.SP25.Api/DataContext.cs
@@ -6,10 +6,6 @@ namespace Selu383.SP25.Api
 {
     public class DataContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SP25-P01-G08;Trusted_Connection=True");
-        }
 
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
     }
--- Selu383.SP25.Api/Program.cs
+++ Selu383.SP25.Api/Program.cs
@@ -1,5 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using NSwag;
+using Microsoft.Extensions.Hosting;
 using System.Reflection.Metadata;
 
 
@@ -12,43 +13,37 @@ namespace Selu383.SP25.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<DbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            builder.Services.AddOpenApiDocument(options => {
-                options.PostProcess = document =>
-                {
-                    document.Info = new OpenApiInfo
-                    {
-                        Version = "v1",
-                        Title = "Development API",
-                    };
-                };
-            });
+
+            builder.Services.AddSwaggerGen();
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
             {
-                app.MapOpenApi();
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<DbContext>();
+                context.Database.Migrate();
             }
 
             if (app.Environment.IsDevelopment())
             {
-                // Add OpenAPI 3.0 document serving middleware
-                // Available at: https://localhost:<5144>/swagger/v1/swagger.json
-                app.UseOpenApi();
-
-                // Add web UIs to interact with the document
-                // Available at: https://localhost:<5144>/swagger
-                app.UseSwaggerUi(); // UseSwaggerUI Protected by if (env.IsDevelopment())
+                //Enable Swagger UI in development
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Selu383 API V1");
+                    c.RoutePrefix = string.Empty;
+                }
+                );
             }
-
-
 
             app.UseHttpsRedirection();
 
