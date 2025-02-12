f005b31e35ace96f3b421f0d89d2b107c33eea06
Cwalt2 <chris.waltersts@gmail.com>
2025-02-12 17:23:56 -0600

    Fixing DataContext
    fixing just the name MyDataContext. In class it was said that the .yml tests won't pass without exact DataContext wording.

--- Selu383.SP25.Api/Program.cs
+++ Selu383.SP25.Api/Program.cs
@@ -10,7 +10,7 @@ namespace Selu383.SP25.Api
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("MyDataContext")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 
             // Add services to the container.
 
